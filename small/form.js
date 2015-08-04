42d5b101bda6e519b0353d38719efa364fae3870
lwxyfer <lwxyfer@gmail.com>
2015-08-04 13:27:17 +0800

    codewars

--- small/form.js
+++ small/form.js
@@ -1,6 +1,6 @@
 var form = document.getElementById("form");
 form[1].focus();
-alert("js检查当前form控件的类型: " +form[1].type);
+//alert("js检查当前form控件的类型: " +form[1].type);
 form[2].disabled = true;
 
 // var  addfunction = {
@@ -17,3 +17,31 @@ url.setAttribute("type","url");
 url.setAttribute("value","setAttribute");
 form.appendChild(email);
 form.appendChild(url);
+
+//富文本编辑的实现
+//由于浏览器同源策略问题，所以先不管ifrmae
+// window.onload = function () {
+//     frames["richedit"].document.designMode= "on";
+// }
+var li = document.getElementsByTagName("li");
+var rich = document.getElementById("rich");
+var bold= document.getElementsByClassName("bold");
+bold[0].onclick = function() {
+    document.execCommand("bold",false,null);
+}
+var a=0;
+bold[1].onclick = function() {
+    
+    if(a==1){
+    document.execCommand("backcolor",false,"fff");
+    a=0;
+    }
+    else if(a==0) {
+        document.execCommand("backcolor",false,"red");
+        a=1;
+    }
+}
+//双击事件
+// bold[1].ondblclick= function() {
+//     document.execCommand("backcolor",false,"#fff")
+// }
